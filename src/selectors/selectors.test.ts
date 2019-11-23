8d794a9d6de1c4f869a86359f57d9d8c90a0f962
Eugene Samonenko <esamonenko@agilevision.io>
2019-11-23 18:09:09 +0200

    updated selectors tests

--- src/selectors/selectors.test.ts
+++ src/selectors/selectors.test.ts
@@ -12,11 +12,12 @@ const state: State = {
     testItem: {
       testField: 'testData',
       items: [
-        { id: '2', data: 'testData2' },
+        { field: 'active', data: 'testData' },
         { id: '1', data: 'testData1' }
       ],
-      itemIndex: { '2': 0, '1': 1 },
-      activeItemId: '1'
+      itemIndex: { active: 0, '1': 1 },
+      selectedItems: { '1': 1 },
+      activeItemId: 'active'
     }
   }
 }
@@ -26,7 +27,7 @@ describe('items selectors', () => {
     it('should return data for given itemType', () => {
       const result = getItems(state, 'testItem')
       expect(result).toEqual([
-        { id: '2', data: 'testData2' },
+        { field: 'active', data: 'testData' },
         { id: '1', data: 'testData1' }
       ])
     })
@@ -69,9 +70,9 @@ describe('items selectors', () => {
   describe('selectors [getActiveItem, getActiveItemId]', () => {
     it('should return active item/id for given itemType', () => {
       const id = getActiveItemId(state, 'testItem')
-      expect(id).toEqual('1')
+      expect(id).toEqual('active')
       const result = getActiveItem(state, 'testItem')
-      expect(result).toEqual({ id: '1', data: 'testData1' })
+      expect(result).toEqual({ field: 'active', data: 'testData' })
     })
     it('should return undefined for unknown itemType', () => {
       const id = getActiveItemId(state, 'fakeItem')
