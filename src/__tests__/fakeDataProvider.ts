5d80debc7a58208ac7b262eceb800ae71edd55f0
Eugene Samonenko <esamonenko@agilevision.io>
2019-11-26 17:51:18 +0200

    improved test for Process helper with SagaTester

--- src/__tests__/fakeDataProvider.ts
+++ src/__tests__/fakeDataProvider.ts
@@ -1,9 +1,7 @@
 import { DataProvider } from '../providers'
-import { TProcessInstance } from '../types'
 
 export class FakeDataProvider extends DataProvider {
-  doRequest(request: any) {
-    console.log(request)
-    return { result: true }
+  provideData(request: any) {
+    return { ...request, result: true }
   }
 }
