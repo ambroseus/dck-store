1a182e9982eeecb4d1fb1c70fa57d616cc24253c
ambroseus <ambroseus@gmail.com>
2019-11-29 16:19:29 +0200

    export actions and selectors explicitly

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as dckSelectors from './dck/selectors'
 
 export { ActionTypes as dckActionTypes, Acts } from './types'
 export { dckActions, dckSelectors }
+export * from './dck/actions'
+export * from './dck/selectors'
 export { dckReducer } from './dck/reducer'
 export { isAction } from './helpers/actions'
 export * from './helpers/processes'
