dad81103fedc40b34ae2d3024830c28da9f518b2
ambroseus <ambroseus@gmail.com>
2019-11-30 12:56:06 +0200

    added items selectors hooks

--- src/items/hooks.ts
+++ src/items/hooks.ts
@@ -1,30 +1,47 @@
-import { useSelector, useDispatch, shallowEqual } from 'react-redux'
+import { useSelector, useDispatch, shallowEqual as shallow } from 'react-redux'
 import { select, dispatcher } from '../helpers/hooks'
 
-import { getItems, getItem, getOptedItem, getOptedItemId } from './selectors'
+import {
+  getItems,
+  getItem,
+  getOptedItem,
+  getOptedItemId,
+  isItemSelected,
+  getSelectedItemIds,
+  getSelectedItems,
+} from './selectors'
+
 import { setItems, optItem } from './actions'
 
-// selectors
+// selectors hooks
 
 export const useItems = (itemType: string): any[] =>
-  useSelector(select(getItems, itemType), shallowEqual)
+  useSelector(select(getItems, itemType), shallow)
 
 export const useItem = (itemType: string, id: string | number): any[] =>
-  useSelector(select(getItem, itemType, id), shallowEqual)
+  useSelector(select(getItem, itemType, id), shallow)
 
 export const useOptedItem = (itemType: string): any =>
-  useSelector(select(getOptedItem, itemType), shallowEqual)
+  useSelector(select(getOptedItem, itemType), shallow)
 
 export const useOptedItemId = (itemType: string): any =>
-  useSelector(select(getOptedItemId, itemType), shallowEqual)
+  useSelector(select(getOptedItemId, itemType), shallow)
 
-// dispatchers
+export const useItemSelected = (
+  itemType: string,
+  id: string | number
+): boolean => useSelector(select(isItemSelected, itemType, id), shallow)
 
-export function useSetItems(itemType: string, items: any[]): any {
-  const dispatch = useDispatch()
-  return dispatcher(dispatch, setItems(itemType, items))
-}
-export function useOptItem(itemType: string, id: string | number): any {
-  const dispatch = useDispatch()
-  return dispatcher(dispatch, optItem(itemType, id))
-}
+export const useSelectedItems = (itemType: string): any[] =>
+  useSelector(select(getSelectedItems, itemType), shallow)
+
+export const useSelectedItemsIds = (itemType: string): any[] =>
+  useSelector(select(getSelectedItemIds, itemType), shallow)
+
+// dispatchers hooks
+
+export const useSetItems = (itemType: string, items: any[]): any =>
+  dispatcher(useDispatch(), setItems(itemType, items))
+
+export const useOptItem = (itemType: string, id: string | number): any =>
+  dispatcher(useDispatch(), optItem(itemType, id))
