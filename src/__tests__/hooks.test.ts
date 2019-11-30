c9025d18fc55182353b99433ff2d440e268a33e1
ambroseus <ambroseus@gmail.com>
2019-11-30 11:41:05 +0200

    added dispatcher hooks tests

--- src/__tests__/hooks.test.ts
+++ src/__tests__/hooks.test.ts
@@ -1,16 +1,32 @@
-import { testHook } from './testHook'
-import { useItems } from '../items/hooks'
-import { cleanup, waitForElement } from '@testing-library/react'
+import { testSelectorHook, testDispatcherHook } from './testHook'
+import { useItems, useSetItems } from '../items/hooks'
+import { cleanup, waitForElement, fireEvent } from '@testing-library/react'
 import { TestItem } from './testData'
 
 afterEach(cleanup)
 
 describe('hooks', () => {
-  it('should render component with hook useItems', async () => {
-    const { getByTestId, getByText } = testHook(() => useItems(TestItem))
-    await waitForElement(() => getByTestId('output'))
-    expect(
-      getByText('[{"id":"1","data":"data1"},{"id":"2","data":"data2"}]')
-    ).toBeDefined()
+  describe('selector useItems', () => {
+    it('should successfully execute', async () => {
+      const { getByTestId, getByText } = testSelectorHook(() =>
+        useItems(TestItem)
+      )
+      await waitForElement(() => getByTestId('testid'))
+      expect(
+        getByText('[{"id":"1","data":"data1"},{"id":"2","data":"data2"}]')
+      ).toBeDefined()
+    })
+  })
+
+  describe('dispatcher useSetItems', () => {
+    it('should successfully execute', async () => {
+      const { getByTestId } = testDispatcherHook(() =>
+        useSetItems(TestItem, [])
+      )
+      let el = await waitForElement(() => getByTestId('testid'))
+      fireEvent.click(el)
+      el = await waitForElement(() => getByTestId('clicked'))
+      expect(el).toBeDefined()
+    })
   })
 })
