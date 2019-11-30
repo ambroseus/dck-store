df8df2e8c2a511cbb2881968c0280aafa810e243
ambroseus <ambroseus@gmail.com>
2019-11-30 12:21:17 +0200

    refactored hooks tests

--- src/__tests__/hooks.test.ts
+++ src/__tests__/hooks.test.ts
@@ -1,4 +1,4 @@
-import { testSelectorHook, testDispatcherHook } from './testHook'
+import { testSelectorHook, testDispatcherHook } from './testHooks'
 import { useItems, useSetItems } from '../items/hooks'
 import { cleanup, waitForElement, fireEvent } from '@testing-library/react'
 import { TestItem } from './testData'
@@ -8,10 +8,7 @@ afterEach(cleanup)
 describe('hooks', () => {
   describe('selector useItems', () => {
     it('should successfully execute', async () => {
-      const { getByTestId, getByText } = testSelectorHook(() =>
-        useItems(TestItem)
-      )
-      await waitForElement(() => getByTestId('testid'))
+      const { getByText } = testSelectorHook(() => useItems(TestItem))
       expect(
         getByText('[{"id":"1","data":"data1"},{"id":"2","data":"data2"}]')
       ).toBeDefined()
@@ -23,9 +20,8 @@ describe('hooks', () => {
       const { getByTestId } = testDispatcherHook(() =>
         useSetItems(TestItem, [])
       )
-      let el = await waitForElement(() => getByTestId('testid'))
-      fireEvent.click(el)
-      el = await waitForElement(() => getByTestId('clicked'))
+      fireEvent.click(getByTestId('testid'))
+      const el = await waitForElement(() => getByTestId('clicked'))
       expect(el).toBeDefined()
     })
   })
